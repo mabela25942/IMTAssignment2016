b31008fca769b1259f26a7088627e00f7d98b098
mabela25942 <mabela25942@gmail.com>
2016-05-25 10:41:00 +0200

    #9 Button Fixed
    Button has been fixed and now is functional..

--- Toxseac/Assets/Code/fishController.cs
+++ Toxseac/Assets/Code/fishController.cs
@@ -42,7 +42,7 @@ public class fishController : MonoBehaviour
 			if (PlayerPrefs.GetInt ("score") < score) {
 				PlayerPrefs.SetInt ("score", score);
 			}
-			//Application.LoadLevel (0);
+			Application.LoadLevel (0);
 		}
 	}
 
